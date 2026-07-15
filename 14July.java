155827a3324fa774362fb1f3847ea8b7864b694a
Lenka Chandini <chandinilenka3@gmail.com>
2026-07-16 00:09:01 +0530

    Initial commit

---
+++ 14July.java
@@ -0,0 +1,31 @@
+class July14 {
+    public static void main(String[] args) {
+        // String is used to store text and must be enclosed in double quotes.
+        String name = "Chandini";
+        int age = 21;
+        System.out.println(name + " is " + age + " years old.");
+
+        byte myAge = 21;
+        short cost = 1000;
+        long salary = 5000000L;
+        System.out.println(myAge);
+        System.out.println("My daily usage cost is " + cost);
+        System.out.println("My salary is " + salary);
+
+
+
+        float price = 15.0f; // float is used for decimal values and must end with f or F
+        double pi = 45.789; // double is used for decimal values, e.g. 56.789
+        System.out.println(price);
+        System.out.println(pi);
+
+        char grade = 'A'; // char is used to store a single character
+        System.out.println(grade);
+
+        boolean passed = true; // boolean is used to store true or false values
+        System.out.println("Yes, " + passed + ", he has passed all subjects.");
+
+
+
+    }
+}
